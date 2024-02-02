ad5c06fb0734768e2b2ff2a0c2d411e7cbd29925
chiduanush <chiduanush1@gmail.com>
2024-02-02 23:46:31 +0530

    Initial Commit

---
+++ Halt/HaltApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaltApp.swift
+//  Halt
+//
+//  Created by Chidu Anush on 02/02/24.
+//
+
+import SwiftUI
+
+@main
+struct HaltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
