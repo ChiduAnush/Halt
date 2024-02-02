44353eb81ae6654cfd5e6322737e934b195961a7
chiduanush <chiduanush1@gmail.com>
2024-02-02 23:52:51 +0530

    Initial Commit

---
+++ Halt/Views/OnBoardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingView.swift
+//  Halt
+//
+//  Created by Chidu Anush on 02/02/24.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    var body: some View {
+        Text("Welcome to the app!")
+    }
+}
+
+#Preview {
+    OnBoardingView()
+}
