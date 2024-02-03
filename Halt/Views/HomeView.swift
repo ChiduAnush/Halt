4685ed654ed7e141a1d35c8b8a102c0a1242e1d6
chiduanush <chiduanush1@gmail.com>
2024-02-03 14:03:32 +0530

    HomeView upcoming event and quote UI

--- Halt/Views/HomeView.swift
+++ Halt/Views/HomeView.swift
@@ -9,10 +9,82 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Hello, Home")
+        
+        
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [Color(.systemIndigo).opacity(0.5), .clear]), startPoint: .topTrailing, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            VStack{
+                HomeTopNextEvent()
+                HomeViewQuote()
+            }
+            
+        }
+        
+        
+        
     }
 }
 
 #Preview {
     HomeView()
 }
+
+
+
+
+
+struct HomeViewQuote: View {
+    var body: some View {
+        HStack{
+            VStack(alignment: .leading){
+                Text("Overcome Distractions,")
+                    .font(.system(size: 45))
+                    .fontWeight(.light)
+                HStack{
+                    Text("Conquer")
+                        .font(.system(size: 50))
+                        .fontWeight(.regular)
+                        .foregroundStyle(Color(.systemIndigo))
+                    Text("your")
+                        .font(.system(size: 45))
+                }
+                Text("Mind.")
+                    .font(.system(size: 45))
+            }
+            .padding(.leading)
+            Spacer()
+
+        }
+
+    }
+}
+
+struct HomeTopNextEvent: View {
+    var body: some View {
+        HStack{
+            Image(systemName: "clock")
+                .font(.title2)
+            Text("iOS Team Gmeet")
+                .fontWeight(.medium)
+            Spacer()
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image(systemName: "chevron.down")
+                    .font(.title3)
+                    .foregroundStyle(Color(.systemIndigo))
+                    
+            })
+        }
+        .padding(.horizontal)
+        
+        Divider()
+            .padding(.horizontal)
+            .padding(.horizontal)
+            .padding(.vertical, 10)
+            .foregroundStyle(Color(.systemGray6))
+        
+        
+    }
+}
+
