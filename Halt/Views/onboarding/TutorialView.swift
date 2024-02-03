19b2c6c229838fb9240a634d6eb4a040a5cab620
chiduanush <chiduanush1@gmail.com>
2024-02-03 08:11:55 +0530

    tutorial View in progress

---
+++ Halt/Views/onboarding/TutorialView.swift
@@ -0,0 +1,18 @@
+//
+//  TutorialView.swift
+//  Halt
+//
+//  Created by Chidu Anush on 03/02/24.
+//
+
+import SwiftUI
+
+struct TutorialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TutorialView()
+}
