b2070edce7189be4f7f96637af1f1bd50e27f280
diyalv <diyavrghs@gmail.com>
2024-02-03 10:56:16 +0530

    Made changes in onboarding

--- Halt/Views/onboarding/OnBoardingView.swift
+++ Halt/Views/onboarding/OnBoardingView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct OnBoardingView: View {
     var body: some View {
         NavigationStack{
-            VStack(spacing: 40){
+            VStack(spacing: 35){
                 Spacer()
                 
                 VStack{
@@ -22,78 +22,82 @@ struct OnBoardingView: View {
                         .font(.subheadline)
                         .fontWeight(.light)
                 }
-                .padding(.vertical, 25)
+                .padding(.vertical, 50)
                 
                 
-                
-                HStack{
-                    Image(systemName: "macbook")
-                        .font(.title)
-                        .foregroundStyle(Color(.systemIndigo))
-                        .padding(.trailing, 10)
-                    VStack(alignment:.leading){
-                        Text("Boost Productivity")
-                            .font(.callout)
-                            .fontWeight(.medium)
-                        Text("Stay focussed and productive by effectively managing distractions.")
-                            .font(.callout)
-                            .fontWeight(.light)
-                            .foregroundStyle(Color(.systemGray))
+                VStack{
+                    HStack{
+                        Image(systemName: "macbook")
+                            .font(.title)
+                            .foregroundStyle(Color(.systemIndigo))
+                            .padding(.trailing, 10)
+                        VStack(alignment:.leading){
+                            Text("Boost Productivity")
+                                .font(.callout)
+                                .fontWeight(.medium)
+                            Text("Stay focussed and productive by effectively managing distractions.")
+                                .font(.callout)
+                                .fontWeight(.light)
+                                .foregroundStyle(Color(.systemGray))
+                        }
                     }
-                }
-                
-                HStack{
-                    Image(systemName: "stopwatch")
-                        .font(.title)
-                        .foregroundStyle(Color(.systemIndigo))
-                        .padding(.horizontal)
-                    VStack(alignment:.leading){
-                        Text("Reclaim your time")
-                            .font(.callout)
-                            .fontWeight(.medium)
-                        Text("Regain control over how your time is spent. ")
-                            .font(.callout)
-                            .fontWeight(.light)
-                            .foregroundStyle(Color(.systemGray))
+                    Spacer()
+                    
+                    HStack{
+                        Image(systemName: "stopwatch")
+                            .font(.title)
+                            .foregroundStyle(Color(.systemIndigo))
+                            .padding(.horizontal)
+                        VStack(alignment:.leading){
+                            Text("Reclaim your time")
+                                .font(.callout)
+                                .fontWeight(.medium)
+                            Text("Regain control over how your time is spent. ")
+                                .font(.callout)
+                                .fontWeight(.light)
+                                .foregroundStyle(Color(.systemGray))
+                        }
                     }
-                }
-                
-                HStack{
-                    Image(systemName: "pencil.line")
-                        .font(.title)
-                        .foregroundStyle(Color(.systemIndigo))
-                        .padding(.horizontal, 19)
-                    VStack(alignment:.leading){
-                        Text("Never forget")
-                            .font(.callout)
-                            .fontWeight(.medium)
-                        Text("Get timely reminders for your tasks and events.")
-                            .font(.callout)
-                            .fontWeight(.light)
-                            .foregroundStyle(Color(.systemGray))
+                    Spacer()
+                    
+                    HStack{
+                        Image(systemName: "pencil.line")
+                            .font(.title)
+                            .foregroundStyle(Color(.systemIndigo))
+                            .padding(.horizontal, 19)
+                        VStack(alignment:.leading){
+                            Text("Never forget")
+                                .font(.callout)
+                                .fontWeight(.medium)
+                            Text("Get timely reminders for your tasks and events.")
+                                .font(.callout)
+                                .fontWeight(.light)
+                                .foregroundStyle(Color(.systemGray))
+                        }
                     }
+                    Spacer()
                 }
                 
                 Spacer()
                 Spacer()
                 
-                NavigationLink(destination: TutorialView()){
-                    Text("Continue")
-                        
-                        .fontWeight(.medium)
-                        .foregroundStyle(Color(.white))
-                        .padding(.horizontal, 100)
-                        .padding(.vertical, 15)
-                        .background(Color(.systemIndigo))
-                        .clipShape(RoundedRectangle(cornerRadius: 15))
-                }
 
                 
-                Spacer()
+  //              Spacer()
 
 
             }
-            .padding()
+            .padding(10)
+            NavigationLink(destination: TutorialView()){
+                Text("Continue")
+                    
+                    .fontWeight(.medium)
+                    .foregroundStyle(Color(.white))
+                    .padding(.horizontal, 100)
+                    .padding(.vertical, 15)
+                    .background(Color(.systemIndigo))
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+            }.padding(.bottom, 60)
         }
         
         
