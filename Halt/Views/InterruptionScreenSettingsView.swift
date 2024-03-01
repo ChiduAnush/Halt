d45556915bff5f76d6326c5a9579797a16fd126a
chiduanush <ca0152@srmist.edu.in>
2024-03-02 00:37:51 +0530

    add interruption start view

--- Halt/Views/InterruptionScreenSettingsView.swift
+++ Halt/Views/InterruptionScreenSettingsView.swift
@@ -20,57 +20,7 @@ struct InterruptionScreenSettingsView: View {
     var body: some View {
         
         Text("settings")
-//        
-//        ZStack {
-//            HStack{
-//                VStack(alignment: .leading){
-//                    Text("Uh oh")
-//                        .font(.system(size: 50))
-//                        .offset(x: showText1 ? 0 : -UIScreen.main.bounds.width/2)
-//                        .opacity(showText1 ? 1 : 0)
-//                        .onAppear() {
-//                            withAnimation(.easeOut(duration: 1)) {
-//                                self.showText1 = true
-//                            }
-//                        }
-//                    
-//                    
-//                    Text("Lorem ipsum set donor amit lorem\nipsum set donor amit")
-//                        .opacity(showText2 ? 1 : 0)
-//                }
-//                
-//                Spacer()
-//            }
-//            .padding(.leading)
-//            .onAppear{
-//                DispatchQueue.main.asyncAfter(deadline: .now() + 1.2) {
-//                    withAnimation {
-//                        showText2 = true
-//                    }
-//                }
-//            }
-//            
-//            if showNewView {
-//                InterruptionTasks()
-//                    .transition(.move(edge: .bottom))
-//                    .animation(.easeInOut(duration: 2))
-//            }
-//            
-//            
-//            
-//            
-//            
-//            
-//        }
-//        .onAppear{
-//            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-////                withAnimation {
-////                    showNewView = true
-////                }
-//                showNewView = true
-//            }
-//        }
-        
+
         
         
         
---
+++ Halt/Views/interruptions/StartInterruptionView.swift
@@ -0,0 +1,81 @@
+//
+//  StartInterruptionView.swift
+//  Halt
+//
+//  Created by user2 on 01/03/24.
+//
+
+import SwiftUI
+
+struct StartInterruptionView: View {
+    
+    @State private var showUhOh = false
+    @State private var showUhOhDesc = false
+    
+    @State private var showInterruptionTasks = false
+    
+    
+    var body: some View {
+        
+        
+        
+        
+        ZStack {
+            
+            
+            HStack{
+                VStack(alignment: .leading, spacing: 10){
+                    Text("Uh oh")
+                        .font(.system(size: 50))
+                        .offset(x: showUhOh ? 0 : -UIScreen.main.bounds.width/2)
+                        .opacity(showUhOh ? 1 : 0)
+                        .onAppear() {
+                            withAnimation(.easeOut(duration: 1)) {
+                                self.showUhOh = true
+                            }
+                            
+                        }
+                    
+                    Text("Lorem ipsum set donor amit lorem\nipsum set donor amit")
+                        .opacity(showUhOhDesc ? 1 : 0)
+                        .offset(x: 1)
+                }
+                
+                Spacer()
+            }
+            .padding(.leading)
+            .onAppear{
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.2) {
+                    withAnimation {
+                        showUhOhDesc = true
+                    }
+                }
+            }
+            
+            if showInterruptionTasks {
+                InterruptionTasks()
+                    .transition(.move(edge: .bottom))
+                    .animation(.easeInOut(duration: 2))
+            }
+            
+            
+            
+            
+        }
+        .onAppear{
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                showInterruptionTasks = true
+            }
+        }
+        
+        
+        
+    }
+}
+
+#Preview {
+    StartInterruptionView()
+}
+
+
+
